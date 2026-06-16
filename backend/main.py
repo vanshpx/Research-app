a2a97e11f1c9e679afd910c013d8796584b2f305
vanshpx <vanshwork2@gmail.com>
2026-06-16 17:04:20 +0530

    some minor cahnges

--- backend/main.py
+++ backend/main.py
@@ -79,4 +79,4 @@ if frontend_dir.exists():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
